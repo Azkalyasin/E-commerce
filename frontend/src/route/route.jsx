5de333820c932ce6984aa92331629ff8496be23f
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-04-11 18:52:13 +0700

    feat: add about and contact route in route

--- frontend/src/route/route.jsx
+++ frontend/src/route/route.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "../App";
-import Home from "../Page/Home.jsx/Home";
+import Home from "../Page/Home/Home";
+import Collection from "../Page/Collection/Collection";
+import About from "../Page/About/About";
+import Contact from "../Page/Contact/Contact";
 
 let router = createBrowserRouter([
   {
@@ -10,9 +13,20 @@ let router = createBrowserRouter([
 
         {
             path:'/',
-            Component: <Home/>
+            Component: Home
         },
-
+        {
+            path:'/Collection',
+            Component: Collection
+        },
+        {
+            path:'/About',
+            Component:About
+        },
+        {
+            path:'/Contact',
+            Component:Contact
+        }
     ]
   },
 ]);
