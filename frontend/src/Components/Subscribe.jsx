2a9cb7689d9684cceb63fde33e9b6ccda6853240
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-04-11 11:11:09 +0700

    feat(Subscribe): add subscribe

---
+++ frontend/src/Components/Subscribe.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <div className="px-4 md:px-6 lg:px-40 mt-60">
+      <div className="flex items-center justify-center flex-col">
+        <div className="flex items-center justify-center md:justify-start gap-2">
+          <p className="text-xl  text-black font-medium">SUBSCRIBE NOW & GET 20% OFF</p>
+        </div>
+        <div className="">
+          <p className="md:text-start text-center pt-2 text-gray-500">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
